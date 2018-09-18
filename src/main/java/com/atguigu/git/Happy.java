3054b0454d69c25ae72f932d192abdb7111f3753
eclipse_user <eclipse_user@163.com>
2018-09-18 10:54:37 +0800

    test conflict left

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -17,7 +17,7 @@ package com.atguigu.git;
 */
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("test...... ");
+		System.out.println("test...left... ");
 	}
 
 }
