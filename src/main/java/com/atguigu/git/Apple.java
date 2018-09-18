a151bde4a75b624062eeaea93e65ffb0303b3ae3
eclipse_user <eclipse_user@163.com>
2018-09-18 10:25:16 +0800

    My first commit in Eclipse

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,20 @@
+/**   
+* @Title: Apple.java 
+* @Package com.atguigu.git 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author  zhufy  
+* @date 2018年9月18日 上午9:48:47 
+* @version V1.0   
+*/
+package com.atguigu.git;
+
+/** 
+* @ClassName: Apple 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author  zhufy  
+* @date 2018年9月18日 上午9:48:47 
+* @version V1.0 
+*/
+public class Apple {
+
+}
---
+++ src/test/java/com/atguigu/test/GoodTest.java
@@ -0,0 +1,20 @@
+/**   
+* @Title: GoodTest.java 
+* @Package com.atguigu.test 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author  zhufy  
+* @date 2018年9月18日 上午9:50:26 
+* @version V1.0   
+*/
+package com.atguigu.test;
+
+/** 
+* @ClassName: GoodTest 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author  zhufy  
+* @date 2018年9月18日 上午9:50:26 
+* @version V1.0 
+*/
+public class GoodTest {
+
+}
